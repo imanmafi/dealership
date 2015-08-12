693ac3f4c036bb3129ed9eb5180176a80fba6869
Andrew Haupt & Iman Mafi <student@epicodus.com>
2015-08-12 09:39:38 -0700

    Add specs for save, .all, and .clear methods

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -13,16 +13,43 @@ describe(Vehicle) do
       expect(test_vehicle.make).to(eq("Hyundai"))
     end
 
-    it("returns the model of the vehicle") do
-      test_vehicle = Vehicle.new("Hyundai", "Elantra", 2010)
-      test_vehicle.save
-      expect(test_vehicle.model).to(eq("Elantra"))
+
+    describe('#model') do
+      it("returns the model of the vehicle") do
+        test_vehicle = Vehicle.new("Hyundai", "Elantra", 2010)
+        test_vehicle.save
+        expect(test_vehicle.model).to(eq("Elantra"))
+      end
     end
 
+  describe('#year') do
     it("returns the year of the vehicle") do
-      test_vehicle = Vehicle.new("Hyundai", "Elantra", 2010)
-      test_vehicle.save
-      expect(test_vehicle.year).to(eq(2010))
+        test_vehicle = Vehicle.new("Hyundai", "Elantra", 2010)
+        test_vehicle.save
+        expect(test_vehicle.year).to(eq(2010))
+      end
     end
   end
+
+  describe('#save') do
+    it("saves a vehicle to the Vehicle array") do
+      test_vehicle = Vehicle.new("Hyundai", "Elantra", 2010)
+      test_vehicle.save
+      expect(Vehicle.all).to(eq([test_vehicle]))
+    end
+  end
+
+  describe('.all') do
+    it("is empty at first") do
+      expect(Vehicle.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears vehicles array") do
+      test_vehicle = Vehicle.new("Hyundai", "Elantra", 2010)
+      test_vehicle.save
+      Vehicle.clear()
+      expect(Vehicle.all).to(eq([]))
+    end
 end
